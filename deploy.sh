1ce77bbce49ad18ed72820fbddeafba1098df100
jbaseball362 <joshua.echevarria21@gmail.com>
2026-03-10 09:12:50 -0500

    Add source file and deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Deploy Fantasy Baseball dues tracker to GitHub Pages
+cd "C:\Programming\Fantasy Baseball 2026"
+cp dues_tracker.html index.html
+git add index.html
+git commit -m "Update dues tracker"
+git push
+echo "Done! Site will refresh at https://jbaseball362.github.io/fantasy-baseball-2026 in ~30 seconds."
